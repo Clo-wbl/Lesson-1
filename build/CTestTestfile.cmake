72bc0cc3675cf91f217515a439215731b96e95e9
ChloeBrice <86777029+ChloeBrice@users.noreply.github.com>
2024-04-24 10:13:27 +0900

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/clobr/OneDrive/Documents/Etudes/INSA/Echanges/KU/Computer Vision/Lesson 1
+# Build directory: C:/Users/clobr/OneDrive/Documents/Etudes/INSA/Echanges/KU/Computer Vision/Lesson 1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
