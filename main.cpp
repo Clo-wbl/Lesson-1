8dc0f7e4dbf4cb0f6568509ce278ae951885866f
ChloeBrice <86777029+ChloeBrice@users.noreply.github.com>
2024-05-08 18:24:02 +0900

    report added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-/** LESSON 4 */
 #include <iostream>
 
 #include <opencv2/opencv.hpp>
@@ -10,77 +9,6 @@
 using namespace std;
 using namespace cv;
 
-int argNum = 30;
-
-int main(int argc, char **argv)
-{
-
-    // Open the video
-    VideoCapture vid("..\\..\\img\\flog.m1V");
-    if (!vid.isOpened())
-    {
-        std::cerr << "Error while opening the video" << std::endl;
-        return -1;
-    }
-
-    // Read video frame by frame
-    Mat frame;
-    do{
-        vid.read(frame); // Put one frame at a time
-        if (frame.empty())
-        {
-            std::cout << "Video ended" << std::endl;
-            break;
-        }
-
-        imshow("Video", frame); // Print frame
-        if (waitKey(25) == 27)  // Each frame is displayed 25ms and escape if ESC key pressed
-        {
-            break;
-        }
-
-    } while (!frame.empty());
-
-    vid.release();
-
-    // Read it a second time but displaying feature points too
-
-    VideoCapture vid2("..\\..\\img\\flog.m1V");
-    if (!vid2.isOpened())
-    {
-        std::cerr << "Error while opening the video" << std::endl;
-        return -1;
-    }
-
-    // Feature points
-    vector<KeyPoint> keypoints;
-    Mat imFeatPt;
-
-    do{
-        vid2 >> frame; // Put one frame at a time
-        if (frame.empty())
-        {
-            std::cout << "Video ended" << std::endl;
-            break;
-        }
-
-        // Fast
-        Ptr<FastFeatureDetector> fast = FastFeatureDetector ::create();
-        fast->detect(frame, keypoints);
-
-        drawKeypoints(frame, keypoints, imFeatPt);
-
-        imshow("Video with keypoints", imFeatPt);
-        if (waitKey(25) == 27) // Each frame is displayed 25ms and escape if ESC key pressed
-        {
-            break;
-        }
-    } while (!frame.empty());
-
-    // Destroy
-    vid2.release();
-    frame.release();
-    imFeatPt.release();
-
-    return 0;
+int main(int, char**){
+    std::cout << "Hello, from report1!\n";
 }
